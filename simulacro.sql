29571c96bf50195810d552a90b66371df9b104c9
benjaminvasquez07 <benjaminvasquez@gmail.com>
2024-05-02 11:19:03 -0300

    termine la actividad 3 del simulacro

--- simulacro.sql
+++ simulacro.sql
@@ -6,4 +6,7 @@ ORDER by title ASC
 SELECT DISTINCT rating FROM film
 ORDER by rating ASC
 
-
+--Actividad 3
+SELECT title, replacement_cost, rating from film
+WHERE rating IN ('PG-13', 'PG')
+ORDER BY replacement_cost DESC, title ASC;
