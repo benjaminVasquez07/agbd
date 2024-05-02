4ab8f2b4c2bab5ef8db589ec01f3cc4c9ed5f496
benjaminvasquez07 <benjaminvasquez@gmail.com>
2024-05-02 11:40:52 -0300

    termine la actividad 4 del simulacro

--- simulacro.sql
+++ simulacro.sql
@@ -10,3 +10,8 @@ ORDER by rating ASC
 SELECT title, replacement_cost, rating from film
 WHERE rating IN ('PG-13', 'PG')
 ORDER BY replacement_cost DESC, title ASC;
+
+--Actividad 4
+SELECT c.city as ciudad, b.country as pais from city c
+JOIN country b on  c.country_id = b.country_id
+order by pais;
