2419dad5b9a6ab7701382ec7f33211dc3fd3512d
benjaminvasquez07 <benjaminvasquez289@gmail.co>
2024-05-08 11:44:59 -0300

    termine la actividad 3

--- simulacros/simulacro_hr.sql
+++ simulacros/simulacro_hr.sql
@@ -6,5 +6,10 @@ ORDER by department_name ASC
 SELECT DISTINCT salary from employees
 order by salary DESC
 
+--Actividad 3
+SELECT job_title, min_salary, max_salary from jobs
+WHERE job_title LIKE '%manager'
+GROUP BY job_title
+order by min_salary,max_salary DESC
 
 
