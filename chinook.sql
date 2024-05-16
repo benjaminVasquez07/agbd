e0ee46ddba523c9516817ae2854df2128af3eecf
benjaminvasquez07 <benjaminvasquez289@gmail.com>
2024-05-16 10:40:26 -0300

    termine el ejercicio 1

--- chinook.sql
+++ chinook.sql
@@ -1,21 +1,29 @@
+--Actividad 1
 SELECT name, Composer, Milliseconds from tracks
 
+--Actividad 2
 SELECT LastName, FirstName, Address, City FROM customers
 
+--Actividad 3
 SELECT name, Milliseconds, Composer FROM tracks 
 WHERE Composer is null and Milliseconds > 2900000
 
+--Actividad 4
 SELECT LastName,FirstName,Company from customers
 WHERE Company is NOT NULL
 
+--Actividad 5
 SELECT DISTINCT BillingCity, BillingState from invoices
 WHERE BillingState is not NULL
 ORDER by BillingCity DESC
 
+--Actividad 6 
 SELECT DISTINCT BillingCity, BillingState from invoices
 WHERE BillingState is not NULL
 ORDER by BillingCity DESC
 
+--Actividad 7
 SELECT t.name, g.name from tracks t join genres g on t.GenreId = g.GenreId
 
+--Actividad 8
 SELECT t.name, g.name from tracks t join genres g on t.GenreId = g.GenreId
--- hr.sql
+++ hr.sql
@@ -22,3 +22,31 @@ Mostrar el nombre del empleado con su puesto de trabajo y su salario máximo, or
 SELECT e.first_name as nombre, j.max_salary FROM employees 
 JOIN jobs j on b.job_id = j.job_id
 ORDER BY max_salary
+
+--Actividad 6
+
+SELECT first_name, last_name,salary,d.department_name FROM employees e 
+JOIN departments d on d.department_id = e.department_id
+ORDER BY first_name,d.department_name DESC;
+
+--Actividad 7
+SELECT e.first_name, e.last_name,l.state_province FROM employees e 
+JOIN departments d on e.department_id = d.department_id
+JOIN locations l on l.location_id = d.location_id
+order by e.first_name, e.last_name DESC
+
+--Actividad 8
+SELECT e.last_name, e.first_name, c.country_name, r.region_name from employees e
+JOIN departments d on e.department_id = d.department_id
+JOIN locations l on l.location_id = d.location_id
+JOIN countries c on l.country_id = c.country_id
+JOIN regions r on c.region_id = r.region_id
+ORDER BY r.region_name ASC
+
+--Actividad 9
+SELECT r.region_name, c.country_name from regions r
+join countries c on r.region_id = c.region_id
+ORDER BY c.country_name DESC
+
+--Actividad 10
+
